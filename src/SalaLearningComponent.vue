b1510bd07652793a49d89a98bb3b3152030b3cc0
adamreaksmey <adaminiature@gmail.com>
2024-07-11 16:32:42 +0700

    added some sample component

---
+++ src/SalaLearningComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sala-component">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SalaLearningComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sala-component {
+  color: blue;
+}
+</style>
