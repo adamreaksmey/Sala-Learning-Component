9ec4b6aa0b65fab15d8f5cd4976ef672378abb89
adamreaksmey <adaminiature@gmail.com>
2024-07-12 14:42:33 +0700

    still unfinished on courseToc imports

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,16 @@
 <template>
-  <div class="w-100 d-flex justify-content-center" id="app">
-    <div class="w-50 bg-white">
-      <div class="h-100 d-flex justify-content-center">
-        <div>
-          <img alt="Vue logo" src="./assets/logo.png" />
-          <SalaLearning :title="'Hello from sala component'" />
-          <BButton>Hello world</BButton>
-          <b-link
-            class="position-fixed fixed-bottom m-0 rounded-0 py-1 text-center"
-            variant="warning"
-          >
-            _____OFFLINE
-          </b-link>
-        </div>
-      </div>
-    </div>
+  <div>
+    <CourseToc />
   </div>
 </template>
 
 <script>
-import SalaLearning from "./components/SalaLearning.vue";
-import { BButton, BLink } from "bootstrap-vue";
+import CourseToc from "./components/SideBar/CourseToc.vue";
 
 export default {
   name: "App",
   components: {
-    SalaLearning,
-    BButton,
-    BLink,
+    CourseToc,
   },
 };
 </script>
